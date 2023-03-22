6cb3f6b62eacfad0527d5d23a392000865f066de
centinela1024tb <centinela1024tb@gmail.com>
2023-03-22 14:40:39 -0600

    [ADD] Se resalta la página actual
    => usamos el componente useLocation de Remix

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,7 +1,15 @@
-import { Link } from '@remix-run/react'
+import { 
+   Link, 
+   useLocation 
+} from '@remix-run/react'
+
 import logo from '../../public/img/logo.svg'
 
 function Header() {
+
+   const location = useLocation();
+   console.log(location.pathname)
+
   return (
    <header className="header">
    <div className="contenedor barra">
@@ -11,18 +19,22 @@ function Header() {
       <div className="navegacion">
          <Link
             to="/"
+            className={ location.pathname === '/' ? 'active' : ''}
          >Inicio</Link>
 
          <Link
             to="/nosotros"
+            className={ location.pathname === '/nosotros' ? 'active' : ''}
          >Nosotros</Link>
 
          <Link
             to="/tienda"
+            className={ location.pathname === '/tienda' ? 'active' : ''}
          >Tienda</Link>
 
          <Link
             to="/blog"
+            className={ location.pathname === '/blog' ? 'active' : ''}
          >Blog</Link>
       </div>
    </div>
