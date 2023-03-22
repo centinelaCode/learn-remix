31b454bbd1076059ac9077d14924c6e32a6c5d61
centinela1024tb <centinela1024tb@gmail.com>
2023-03-22 15:59:39 -0600

    [ADD] Se hace petieciones a la API de 2 formas, en la funcion loader o utilizando un model
    => al utilizar un model debes nombrar el archivo con extención .server.js

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,23 @@
+import { getGuitarras } from '../models/guitarras.server';
+
+export async function loader() {
+  // ==== forma de hacer una petición a una api ====
+  // const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+  // const resultado = await respuesta.json();
+
+
+  // ==== Otra forma de hacer una petición a una api ====
+  const guitarras = await getGuitarras();
+  // console.log(guitarras) // solo se muestran en la consola del servidor
+  
+  return guitarras;
+}
+
 
 
 function Tienda() {
+  
+
    return (
      <div>Tienda</div>
    )
