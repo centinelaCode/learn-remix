6b63c90ae8d44f8245c3d693467d8c00ff4428dc
centinela1024tb <centinela1024tb@gmail.com>
2023-03-23 14:04:04 -0600

    [ADD] Se implementa otra función para la solicitud a la API pero filtrando por url
    => le enviamos el param url para hacer el filtro

--- app/models/guitarras.server.js
+++ app/models/guitarras.server.js
@@ -1,4 +1,10 @@
 export async function getGuitarras() {
-   const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+   const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);   
+   return await respuesta.json();  
+}
+
+export async function getGuitarraUrl(url) {
+   const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`);   
+   //http://localhost:1337/api/guitarras?filters[url]=beck&populate=imagen
    return await respuesta.json();  
 }
