3739bde810c969d31e124aecb03e1dabd5f76630
centinela1024tb <centinela1024tb@gmail.com>
2023-03-23 14:30:06 -0600

    [ADD] Se finaliza la maquetación para mostrar la guitarra que se selecciono d ela tienda
    => Se hacen un pequeño ajuste a los estilos a la clase .guitarra poniendo un max-width de 70rem
    => Y un margin 0 auto para centra

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -1,7 +1,18 @@
 import { useLoaderData } from '@remix-run/react'
 
 import { getGuitarraUrl } from '~/models/guitarras.server';
+import styles from '~/styles/guitarras.css'
 
+
+export function links() {
+   return [
+     {
+       rel: 'stylesheet',
+       href: styles
+     }
+   ]
+ }
+ 
 
 export async function loader({params}) {
    // obtenemos el params de la url
@@ -12,12 +23,21 @@ export async function loader({params}) {
 }
 
 function Guitarra() {
-
+   // obtenemos los dato del loader
    const guitarra = useLoaderData();
-   console.log(guitarra.data[0].attributes.nombre)
+   console.log(guitarra.data[0].attributes)
+   const { nombre, descripcion, imagen, precio} = guitarra.data[0].attributes;
 
    return (
-      <div>xxx</div>
+      <main className="contendor guitarra">
+         <img className='imagen' src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} />
+
+         <div className="contenido">
+            <h3>{nombre}</h3>
+            <p className="texto">{descripcion}</p>
+            <p className="precio">${precio}</p>
+         </div>
+      </main>
    )
 }
 
