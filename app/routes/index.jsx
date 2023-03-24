c338f544eb6af181cfbc1526a08634a59b1051ed
centinela1024tb <centinela1024tb@gmail.com>
2023-03-24 13:17:45 -0600

    [ADD] Se agrega la seccion de Curso
    => Se consume de la API
    => Se aplican los estilos
    => Se coloca la imagen de que se consume de la API como fondo mediante <styles jsx=true> desde el componente Curso

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,11 +2,14 @@ import { useLoaderData } from '@remix-run/react'
 
 import { getGuitarras } from '~/models/guitarras.server'
 import { getPosts } from '~/models/posts.server'
+import { getCurso } from '~/models/curso.server'
 import ListadoGuitarras from '~/components/listado-guitarras'
 import ListadoPosts from '~/components/listado-posts'
+import Curso from '~/components/curso'
 
 import stylesGuitarras from '~/styles/guitarras.css'
 import stylesPosts from '~/styles/blog.css'
+import stylesCurso from '~/styles/curso.css'
 
 export function meta() {
 
@@ -21,26 +24,33 @@ export function links() {
     {
       rel: 'stylesheet',
       href: stylesPosts,
-    }
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesCurso,
+    },
+    
   ]
 }
 
 export async function loader() {
 
-  const [guitarras, posts] = await Promise.all([getGuitarras(), getPosts()])  
+  const [guitarras, posts, curso] = await Promise.all([getGuitarras(), getPosts(), getCurso()])  
 
   return {
     guitarras: guitarras.data,
     posts: posts.data,
+    curso: curso.data,
   }
 }
 
 
 function Index() {
 
-  const { guitarras, posts } = useLoaderData();
+  const { guitarras, posts, curso } = useLoaderData();
   // console.log(guitarras)
   // console.log(posts)
+  console.log(curso.attributes)
 
 
 
@@ -51,6 +61,12 @@ function Index() {
           guitarras={guitarras}
         />       
       </main>
+
+
+      <Curso 
+        curso={curso.attributes}
+      />
+
 
       <section className="contenedor">
         <ListadoPosts 
