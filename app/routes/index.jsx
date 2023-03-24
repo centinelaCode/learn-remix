cb8c6f36636d8edb91d91a87392c358fbacc5117
centinela1024tb <centinela1024tb@gmail.com>
2023-03-24 12:41:23 -0600

    [ADD] Se crea componente ListadoPosts para reutilizarlo en el index y blog

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -3,7 +3,10 @@ import { useLoaderData } from '@remix-run/react'
 import { getGuitarras } from '~/models/guitarras.server'
 import { getPosts } from '~/models/posts.server'
 import ListadoGuitarras from '~/components/listado-guitarras'
+import ListadoPosts from '~/components/listado-posts'
+
 import stylesGuitarras from '~/styles/guitarras.css'
+import stylesPosts from '~/styles/blog.css'
 
 export function meta() {
 
@@ -14,6 +17,10 @@ export function links() {
     {
       rel: 'stylesheet',
       href: stylesGuitarras,
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesPosts,
     }
   ]
 }
@@ -42,8 +49,14 @@ function Index() {
       <main className="contenedor">
         <ListadoGuitarras 
           guitarras={guitarras}
-        />
+        />       
       </main>
+
+      <section className="contenedor">
+        <ListadoPosts 
+          posts={posts}
+        />
+      </section>
     </>
   )
 }
