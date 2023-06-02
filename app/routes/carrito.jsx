ce38b012851524a5b65e4b7e572e073c1f427fd7
centinela1024tb <centinela1024tb@gmail.com>
2023-06-02 13:45:30 -0600

    [ADD] Se crea la estructura del shooping car

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,9 +1,44 @@
+import styles from '~/styles/carrito.css'
+
+export function meta() {
+   return (
+     {      
+       title: 'GuitarLA - Carrito de Compras',
+       description: 'Venta de guitarras, música,blog, tienda..' 
+     }     
+   )
+ }
+
+export function links() {
+   return [
+     {
+       rel: 'stylesheet',
+       href: styles
+     }
+   ]
+ }
 
 
 function Carrito() {
-  return (
-    <div>carrito</div>
-  )
+   return (
+      <main className="contenedor">
+         <h1 className="heading">Carrito de Compras</h1>
+
+         <div className="contenido">
+            <div className="carrito">
+               <h2>Articulos</h2>
+
+            </div>
+
+            <aside className="resumen">
+               <h3>Resumen del Pedido</h3>
+               <p>Total a pagar: $</p>
+            </aside>
+         </div>
+
+         
+      </main>      
+   )
 }
 
 export default Carrito
