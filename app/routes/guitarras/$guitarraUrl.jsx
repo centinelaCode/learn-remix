224b7345f5a68f8447c7ee6e38aceccc451423ae
centinela1024tb <centinela1024tb@gmail.com>
2023-06-02 15:49:02 -0600

    [ADD] se agrega el evento onSubmit que ejecuta handleSubmit, crea el objeto con la guitarra que se agrego al shooping-car

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -64,6 +64,26 @@ function Guitarra() {
 
    const { nombre, descripcion, imagen, precio} = guitarra.data[0].attributes;
 
+   const handleSubmit = e => {
+      e.preventDefault();
+
+      // valida si no se ha seleccionado una cantidad
+      if(cantidad < 1) {
+         alert('Debes seleccionar una cantidad');
+         return
+      }
+
+      const guitarraSeleccionada = {
+         id: guitarra.data[0].id,
+         imagen: imagen.data.attributes.url,
+         nombre,
+         precio,
+         cantidad
+      }
+
+      console.log(guitarraSeleccionada)
+   }
+
    return (
       <main className="contendor guitarra">
          <img className='imagen' src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} />
@@ -73,13 +93,13 @@ function Guitarra() {
             <p className="texto">{descripcion}</p>
             <p className="precio">${precio}</p>
 
-            <form className='formulario'>
+            <form onSubmit={ handleSubmit } className='formulario'>
                <label htmlFor='cantidad'>Cantidad</label>
                <select 
                   onChange={ e => setCantidad(+e.target.value)}
                   id='cantidad'
                >
-                  <option value="">-- Seleccione --</option>
+                  <option value="0">-- Seleccione --</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
